9664d3fd84d157f516a5210ddc4bb1ea7c89c4a8
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-02 21:23:54 +0700

    chore: timeoff_requests file migartion

---
+++ database/migrations/2022_08_02_130104_create_timeoff_requests_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timeoff_requests', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('organization_id')->references('id')->on('organizations');
+            $table->foreignUuid('timeoff_id')->references('id')->on('timeoffs');
+            $table->foreignUuid('employee_id')->references('id')->on('employees');
+
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->enum('status', [
+                'request',
+                'rejected',
+                'approved',
+            ])->default('paid');
+            $table->text('note');
+            $table->foreignUuid('approval_employee_id')->nullable()->references('id')->on('employees');
+            $table->text('note_reject')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timeoff_requests');
+    }
+};
