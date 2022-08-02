9a40b611d19a8c2791c1b9f5834e562d72744802
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-02 21:23:28 +0700

    chore: timeoffs file migartion

---
+++ database/migrations/2022_08_02_125111_create_timeoffs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timeoffs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('organization_id')->references('id')->on('organizations');
+            $table->string('name');
+            $table->enum('type', [
+                'paid',
+                'unpaid',
+            ])->default('paid');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timeoffs');
+    }
+};
